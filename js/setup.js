cf958af2de3f280e3ed219377ee9eb9c804ca43a
bezal <bezalel@outlook.co.il>
2018-10-03 01:04:21 +0300

    #14 Учебный проект: одеть Надежду - исправления замечаний https://github.com/htmlacademy-javascript/867933-code-and-magick/pull/4#discussion_r221647229

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,17 @@
   var blockSetup = document.querySelector('.setup');
   blockSetup.classList.remove('hidden');
 
+  var setupClose = blockSetup.querySelector('.setup-close');
+
+  // по заданию у класса setup-open position absolute и мышкой его не поймать, поставил обработчик на иконку
+  var setupIcon = document.querySelector('.setup-open-icon');
+
+  var coatColor = blockSetup.querySelector('.wizard-coat');
+  var fireballWraper = blockSetup.querySelector('.setup-fireball-wrap');
+  var wizardEyes = blockSetup.querySelector('.wizard-eyes');
+
+  var domInputName = blockSetup.querySelector('input[name=username]');
+
   // Mock
   var NAME = [
     'Иван',
@@ -44,28 +55,38 @@
     'green'
   ];
 
-  function deleteDuplicates(value, arr) {
+  var properties = {
+    names: NAME.slice(),
+    surnames: SURNAME.slice(),
+    coats: COAT.slice(),
+    eyes: EYE.slice()
+  };
+
+  var wizards = [];
+
+  var deleteDuplicates = function (value, arr) {
     for (var i = 0; i < arr.length; i++) {
       if (arr[i] === value) {
         arr.splice(i, 1);
       }
     }
-  }
+  };
 
-  function randArrElement(arr) {
+  var randArrElement = function (arr) {
     return Math.floor(Math.random() * arr.length);
-  }
+  };
 
-  function generateElements() {
+  var generateElements = function () {
     // Generate blocks from template
     var wizardTemplate = document.querySelector('#similar-wizard-template');
+    var simularListContainer = false;
     if (wizardTemplate) {
       var wizardNode = wizardTemplate.content;
 
       // не уверен что так правильно. Идея была такая создать createDocumentFragment, в него циклом напихать ноды из шаблона
       // и потом добавить разом в дом
 
-      var simularListContainer = document.createDocumentFragment();
+      simularListContainer = document.createDocumentFragment();
 
       for (var i = 0; i < wizards.length; i++) {
         wizardNode.querySelector('.setup-similar-label').innerHTML = wizards[i].name();
@@ -73,50 +94,42 @@
         wizardNode.querySelector('.wizard-eyes').setAttribute('fill', wizards[i].eyesColor());
         simularListContainer.appendChild(wizardNode.cloneNode(true));
       }
-      return simularListContainer;
-    } else {
-      return false;
     }
-  }
-
-  function renderSimularWizards(simularListContainer) {
-    document.querySelector('.setup-similar-list').appendChild(simularListContainer);
-    document.querySelector('.setup-similar').classList.remove('hidden');
-  }
-
-  // запуск
-  var properties = {
-    names: NAME.slice(),
-    surnames: SURNAME.slice(),
-    coats: COAT.slice(),
-    eyes: EYE.slice()
+    return simularListContainer;
   };
-  var wizards = [];
-  for (var i = 0; i < 4; i++) {
-    wizards.push({
-      name: function () {
-        var name = properties.names[randArrElement(properties.names)];
-        var surname = properties.surnames[randArrElement(properties.surnames)];
-        deleteDuplicates(name, properties.names);
-        deleteDuplicates(surname, properties.surnames);
-        return name + ' ' + surname;
-      },
-      coatColor: function () {
-        var coat = properties.coats[randArrElement(properties.coats)];
-        deleteDuplicates(coat, properties.coats);
-        return coat;
-      },
-      eyesColor: function () {
-        var eye = properties.eyes[randArrElement(properties.eyes)];
-        deleteDuplicates(eye, properties.eyes);
-        return eye;
-      }
-    });
-  }
 
-  // Обработка событий, всплывающее окно
+  var renderSimularWizards = function (simularListContainer) {
+    var domSetupsimilar = document.querySelector('.setup-similar');
+    domSetupsimilar.querySelector('.setup-similar-list').appendChild(simularListContainer);
+    domSetupsimilar.classList.remove('hidden');
+  };
+
+  var generateWizards = function () {
+    for (var i = 0; i < 4; i++) {
+      wizards.push({
+        name: function () {
+          var name = properties.names[randArrElement(properties.names)];
+          var surname = properties.surnames[randArrElement(properties.surnames)];
+          deleteDuplicates(name, properties.names);
+          deleteDuplicates(surname, properties.surnames);
+          return name + ' ' + surname;
+        },
+        coatColor: function () {
+          var coat = properties.coats[randArrElement(properties.coats)];
+          deleteDuplicates(coat, properties.coats);
+          return coat;
+        },
+        eyesColor: function () {
+          var eye = properties.eyes[randArrElement(properties.eyes)];
+          deleteDuplicates(eye, properties.eyes);
+          return eye;
+        }
+      });
+    }
+  };
+
   var onSetupClick = function () {
-    var setupPopUpClasses = document.querySelector('.setup').classList;
+    var setupPopUpClasses = blockSetup.classList;
     setupPopUpClasses.forEach(function (itemClass) {
       if (itemClass === 'hidden') {
         setupPopUpClasses.remove('hidden');
@@ -126,62 +139,60 @@
     });
   };
 
-  // точка входа
+  var onKeyDownESC = function (evt) {
+    if (evt.keyCode === 27) {
+      onSetupClick();
+    }
+  };
+
+  var onKeyDownEnter = function (evt) {
+    if (evt.keyCode === 13) {
+      onSetupClick();
+    }
+  };
+
+  // запуск
+  generateWizards();
+
   var elements = generateElements();
   if (elements) {
     renderSimularWizards(elements);
   }
 
-  var setupClose = document.querySelector('.setup-close');
-  setupClose.setAttribute('tabindex', '0');
-
-  // по заданию у класса setup-open position absolute и мышкой его не поймать, поставил обработчик на иконку
-  var setupIcon = document.querySelector('.setup-open-icon');
-  setupIcon.setAttribute('tabindex', '0');
-
-  // Валидация, непонятно правда в каком месте тут валидация, разве что паттерн добавить, в html и так уже почти все есть для задания
-  // в задании про setCustomValidity ничего не нашел, только максимальная и мин длинна имени, которая максимальная уже проставлена в html
-  var inputName = document.querySelector('input[name=username]');
-  inputName.setAttribute('minlength', '2');
-
-  // генерация цветов мага по нажатию
-  var coatColor = document.querySelector('.wizard-coat');
-  var fireballWraper = document.querySelector('.setup-fireball-wrap');
-  var wizardEyes = document.querySelector('.wizard-eyes');
-
-
   // эвенты
   setupClose.addEventListener('click', onSetupClick);
+  setupClose.addEventListener('keydown', onKeyDownEnter);
+
   setupIcon.addEventListener('click', onSetupClick);
 
-  setupIcon.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === 13) {
-      onSetupClick();
-    }
+  setupIcon.addEventListener('keydown', onKeyDownEnter);
+
+  document.addEventListener('keydown', onKeyDownESC);
+
+  domInputName.addEventListener('focus', function () {
+    document.removeEventListener('keydown', onKeyDownESC);
   });
 
-  document.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === 27) {
-      onSetupClick();
-    }
+  domInputName.addEventListener('blur', function () {
+    document.addEventListener('keydown', onKeyDownESC);
   });
 
   coatColor.addEventListener('click', function () {
     var color = COAT[randArrElement(COAT)];
-    document.querySelector('input[name=coat-color]').value = color;
+    blockSetup.querySelector('input[name=coat-color]').value = color;
     coatColor.setAttribute('style', 'fill: ' + color);
   });
 
   fireballWraper.addEventListener('click', function () {
     var colors = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
     var color = colors[randArrElement(colors)];
-    document.querySelector('input[name=fireball-color]').value = color;
+    blockSetup.querySelector('input[name=fireball-color]').value = color;
     fireballWraper.setAttribute('style', 'background-color: ' + color);
   });
 
   wizardEyes.addEventListener('click', function () {
     var color = EYE[randArrElement(EYE)];
-    document.querySelector('input[name=eyes-color]').value = color;
+    blockSetup.querySelector('input[name=eyes-color]').value = color;
     wizardEyes.setAttribute('style', 'fill: ' + color);
   });
 
